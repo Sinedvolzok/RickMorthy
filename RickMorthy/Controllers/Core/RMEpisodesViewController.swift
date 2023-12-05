5741bfae0b65c5d53ca1ad9f5420e4abccb8a3af
Sinedvolzok <sinedvolzok@gmail.com>
2023-12-05 09:33:48 +0300

    Create project add screens

--- RickMorthy/Controllers/Core/RMEpisodesViewController.swift
+++ RickMorthy/Controllers/Core/RMEpisodesViewController.swift
@@ -11,8 +11,8 @@ final class RMEpisodesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        title = "Episodes"
     }
 
 }
